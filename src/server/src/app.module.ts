ba7eefb1d4462975678b65cf5b00bbcaa73feb50
dknguyen176 <dknguyen176@gmail.com>
2022-11-09 01:36:27 +0700

    add swagger + mongoose

--- src/server/src/app.module.ts
+++ src/server/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.dev',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.phxi29l.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    ),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
