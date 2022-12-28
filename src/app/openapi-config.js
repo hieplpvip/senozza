32815e65cd3758f57a38047a90851a0508be5376
Le Bao Hiep <baohiep2013@gmail.com>
2022-12-28 15:17:47 +0700

    app: add commands for generating api

---
+++ src/app/openapi-config.js
@@ -0,0 +1,27 @@
+/** @type {import("@rtk-query/codegen-openapi").ConfigFile} */
+const config = {
+  schemaFile: 'http://localhost:3001/api-json',
+  apiFile: './src/features/api/apiSlice.ts',
+  apiImport: 'apiSlice',
+  outputFile: './generated_apis/default.ts',
+  outputFiles: {
+    './generated_apis/user.ts': {
+      filterEndpoints: [/^user/],
+    },
+    './generated_apis/class.ts': {
+      filterEndpoints: [/^class/],
+    },
+    './generated_apis/feed.ts': {
+      filterEndpoints: [/^feed/],
+    },
+    './generated_apis/post.ts': {
+      filterEndpoints: [/^post/],
+    },
+    './generated_apis/auth.ts': {
+      filterEndpoints: [/^auth/],
+    },
+  },
+  hooks: true,
+};
+
+module.exports = config;
