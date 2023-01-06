2dee7fce339e152842eb1287ead3b0e6ee86c924
leminhhuan72 <leminhhuan72@gmail.com>
2023-01-06 14:09:10 +0700

    app: ui for post and comment option

--- src/app/src/features/class/components/PostDetail.tsx
+++ src/app/src/features/class/components/PostDetail.tsx
@@ -1,5 +1,14 @@
-import { PencilIcon } from '@heroicons/react/24/solid';
-import { ChevronDoubleUpIcon } from '@heroicons/react/24/solid';
+import { Fragment } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import {
+  CheckBadgeIcon,
+  ChevronDownIcon,
+  ChevronDoubleUpIcon,
+  EllipsisHorizontalIcon,
+  MapPinIcon,
+  PencilSquareIcon,
+  TrashIcon,
+} from '@heroicons/react/24/solid';
 import { Spinner, useDisclosure } from '@chakra-ui/react';
 import { MacScrollbar } from 'mac-scrollbar';
 import TimeAgo from 'react-timeago';
@@ -7,36 +16,13 @@ import TimeAgo from 'react-timeago';
 import CreateCommentModal from './CreateCommentModal';
 import EditPostModal from './EditPostModal';
 import { useGetAllCommentsByPostQuery, useGetPostByIdQuery } from '../../api';
-import { useUserProfile } from '../../../app/hooks';
+import { useUserProfile, useIsInstructor } from '../../../app/hooks';
 import { MarkdownPreview } from '../../../components/Markdown';
-import { CommentDto } from '../../../interface';
-
-function Comment({ comment }: { comment: CommentDto }) {
-  return (
-    <div className='mx-auto mt-3 w-full flex-col border-b-2 border-r-2 border-gray-200 bg-white p-4 sm:rounded-lg sm:shadow-sm'>
-      <div className='flex flex-row'>
-        <div className='flex flex-col justify-center'>
-          <button>
-            <ChevronDoubleUpIcon className='h-5 w-5 text-gray-500' aria-hidden='true' />
-            <span className='text-gray-500'>{comment.vote}</span>
-          </button>
-        </div>
-        <div className='flex-col'>
-          <div className='flex flex-1 items-center font-bold leading-tight'>
-            <img className='mx-2 h-9 w-9 rounded-full border-2 border-gray-300' src={comment.user.imgUrl} />
-            {comment.user.firstName} {comment.user.lastName}
-            <span className='ml-1 font-normal text-gray-500'>
-              answered <TimeAgo date={new Date(comment.createdDate)} />
-            </span>
-          </div>
-          <div className='flex-1 px-2 text-sm font-medium leading-loose text-gray-600'>{comment.content}</div>
-        </div>
-      </div>
-    </div>
-  );
-}
+import { classNames } from '../../../utils';
 
 function CommentBox({ postId }: { postId: string }) {
+  const userProfile = useUserProfile();
+  const isInstructor = useIsInstructor();
   const { data, isSuccess } = useGetAllCommentsByPostQuery({ postId, sortBy: 'createdDate' });
   const { isOpen: isModalOpen, onOpen: onModalOpen, onClose: onModalClose } = useDisclosure();
 
@@ -65,9 +51,115 @@ function CommentBox({ postId }: { postId: string }) {
       {isSuccess && (
         <MacScrollbar as='section' className='min-h-0 flex-1'>
           <div className='container mx-auto px-0 sm:px-5'>
-            {data.map((comment) => (
-              <Comment key={comment._id} comment={comment} />
-            ))}
+            {data.map((comment) => {
+              const isAuthor = comment.user._id === userProfile._id;
+
+              return (
+                <div
+                  className='mx-auto mt-3 w-full flex-col border-b-2 border-r-2 border-gray-200 bg-white p-4 sm:rounded-lg sm:shadow-sm'
+                  key={comment._id}>
+                  <div className='flex flex-row'>
+                    <div className='flex flex-col justify-center'>
+                      <button>
+                        <ChevronDoubleUpIcon className='h-5 w-5 text-gray-500' aria-hidden='true' />
+                        <span className='text-gray-500'>{comment.vote}</span>
+                      </button>
+                    </div>
+                    <div className='grow flex-col'>
+                      <div className='flex items-center justify-between leading-tight'>
+                        <div className='flex items-center'>
+                          <img
+                            className='mx-2 h-9 w-9 rounded-full border-2 border-gray-300'
+                            src={comment.user.imgUrl}
+                          />
+                          <span className='font-bold'>
+                            {comment.user.firstName} {comment.user.lastName}
+                          </span>
+                          <span className='ml-1 font-normal text-gray-500'>
+                            answered <TimeAgo date={new Date(comment.createdDate)} />
+                          </span>
+                        </div>
+                        {(isInstructor || isAuthor) && (
+                          <Menu as='div' className='relative inline-block text-left'>
+                            <Menu.Button className='inline-flex w-full justify-center bg-white p-1 text-sm font-medium text-gray-700'>
+                              <EllipsisHorizontalIcon className='h-5 w-5' aria-hidden='true' />
+                            </Menu.Button>
+
+                            <Transition
+                              as={Fragment}
+                              enter='transition ease-out duration-100'
+                              enterFrom='transform opacity-0 scale-95'
+                              enterTo='transform opacity-100 scale-100'
+                              leave='transition ease-in duration-75'
+                              leaveFrom='transform opacity-100 scale-100'
+                              leaveTo='transform opacity-0 scale-95'>
+                              <Menu.Items className='absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                                <div className='py-1'>
+                                  {isInstructor && (
+                                    <Menu.Item>
+                                      {({ active }) => (
+                                        <button
+                                          onClick={onModalOpen}
+                                          className={classNames(
+                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                            'group flex w-full items-center px-4 py-2 text-sm',
+                                          )}>
+                                          <CheckBadgeIcon
+                                            className='mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500'
+                                            aria-hidden='true'
+                                          />
+                                          Mark as correct
+                                        </button>
+                                      )}
+                                    </Menu.Item>
+                                  )}{' '}
+                                  {isAuthor && (
+                                    <Menu.Item>
+                                      {({ active }) => (
+                                        <button
+                                          onClick={onModalOpen}
+                                          className={classNames(
+                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                            'group flex w-full items-center px-4 py-2 text-sm',
+                                          )}>
+                                          <PencilSquareIcon
+                                            className='mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500'
+                                            aria-hidden='true'
+                                          />
+                                          Edit
+                                        </button>
+                                      )}
+                                    </Menu.Item>
+                                  )}
+                                  <Menu.Item>
+                                    {({ active }) => (
+                                      <button
+                                        className={classNames(
+                                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                          'group flex w-full items-center px-4 py-2 text-sm',
+                                        )}>
+                                        <TrashIcon
+                                          className='mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500'
+                                          aria-hidden='true'
+                                        />
+                                        Delete
+                                      </button>
+                                    )}
+                                  </Menu.Item>
+                                </div>
+                              </Menu.Items>
+                            </Transition>
+                          </Menu>
+                        )}
+                      </div>
+                      <div className='flex-1 px-2 text-sm font-normal leading-loose text-gray-600'>
+                        {comment.content}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </MacScrollbar>
       )}
@@ -79,6 +171,7 @@ function CommentBox({ postId }: { postId: string }) {
 
 export default function PostDetail({ postId }: { postId: string }) {
   const userProfile = useUserProfile();
+  const isInstructor = useIsInstructor();
   const { isOpen: isModalOpen, onOpen: onModalOpen, onClose: onModalClose } = useDisclosure();
   const { data: post, isSuccess } = useGetPostByIdQuery(postId, { skip: !postId });
 
@@ -94,6 +187,8 @@ export default function PostDetail({ postId }: { postId: string }) {
     );
   }
 
+  const isAuthor = post.question.user._id === userProfile._id;
+
   return (
     <>
       {isModalOpen && (
@@ -106,7 +201,7 @@ export default function PostDetail({ postId }: { postId: string }) {
         />
       )}
       <div className='flex flex-col py-4 px-4'>
-        <div className='mb-4 flex items-center justify-between space-x-4 pb-2 '>
+        <div className='mb-4 flex items-center justify-between space-x-4 pb-2'>
           <div>
             <div className='flex flex-1 items-center font-bold leading-tight'>
               <img className='mr-2 h-7 w-7 rounded-full border-2 border-gray-300' src={post.question.user.imgUrl} />
@@ -122,16 +217,77 @@ export default function PostDetail({ postId }: { postId: string }) {
               </div> */}
             </div>
           </div>
-          {post.question.user._id === userProfile._id && (
-            <div className='flex space-x-3'>
-              <button
-                type='button'
-                onClick={onModalOpen}
-                className='inline-flex justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50'>
-                <PencilIcon className='-ml-1 mr-2 h-5 w-5 text-gray-400' aria-hidden='true' />
-                <span>Edit</span>
-              </button>
-            </div>
+          {(isInstructor || isAuthor) && (
+            <Menu as='div' className='relative inline-block text-left'>
+              <Menu.Button className='inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50'>
+                Options
+                <ChevronDownIcon className='-mr-1 ml-2 h-5 w-5' aria-hidden='true' />
+              </Menu.Button>
+
+              <Transition
+                as={Fragment}
+                enter='transition ease-out duration-100'
+                enterFrom='transform opacity-0 scale-95'
+                enterTo='transform opacity-100 scale-100'
+                leave='transition ease-in duration-75'
+                leaveFrom='transform opacity-100 scale-100'
+                leaveTo='transform opacity-0 scale-95'>
+                <Menu.Items className='absolute right-0 mt-2 w-48 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                  {isInstructor && (
+                    <div className='py-1'>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            className={classNames(
+                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                              'group flex w-full items-center px-4 py-2 text-sm',
+                            )}>
+                            <MapPinIcon
+                              className='mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500'
+                              aria-hidden='true'
+                            />
+                            Pin to top
+                          </button>
+                        )}
+                      </Menu.Item>
+                    </div>
+                  )}
+                  <div className='py-1'>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          onClick={onModalOpen}
+                          className={classNames(
+                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                            'group flex w-full items-center px-4 py-2 text-sm',
+                          )}>
+                          <PencilSquareIcon
+                            className='mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500'
+                            aria-hidden='true'
+                          />
+                          Edit
+                        </button>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          className={classNames(
+                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                            'group flex w-full items-center px-4 py-2 text-sm',
+                          )}>
+                          <TrashIcon
+                            className='mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500'
+                            aria-hidden='true'
+                          />
+                          Delete
+                        </button>
+                      )}
+                    </Menu.Item>
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </Menu>
           )}
         </div>
 
